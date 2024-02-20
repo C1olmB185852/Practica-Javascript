eecdb9839a97d897c44e9ae398adf553beef6231
C1olmB185852 <dan.ielestebanmat@gmail.com>
2024-02-20 17:26:13 -0500

    .

--- tipos-de-datos-primitivos-null-undefined-symbol-bigint.js
+++ tipos-de-datos-primitivos-null-undefined-symbol-bigint.js
@@ -2,3 +2,25 @@
 const snoopy = null
 console.log(snoopy);
 console.log(typeof snoopy)
+
+// Undefined
+let name
+console.log(name);
+
+// Symbol 
+const uniqueId = Symbol('id')
+const symbol1 = Symbol(1)
+const symbol2 = Symbol(1)
+console.log(symbol1 === symbol2);
+
+const ID = Symbol('id')
+let user = {}
+user[ID] = '1234'
+console.log(user);
+
+// BigInt
+const bigNumer = 12345678998745632145879456665556n
+console.log(bigNumer);
+
+const numberOfParticlesInTheUniverse = 10000000000000000000000000000000000000n
+console.log(numberOfParticlesInTheUniverse);
