5b54a8bf5f2fa3f46ebf1ffe8852dc8a94e9b4d4
C1olmB185852 <dan.ielestebanmat@gmail.com>
2024-03-04 06:49:52 -0500

    .

---
+++ Operadores-de-comparación.js
@@ -0,0 +1,21 @@
+/* 
+
+Operaciones de compracion
+
+==
+===
+!=
+!==
+>
+<
+>=
+<=
+
+*/
+
+const a = 10;
+const b = 20;
+const c = "10"
+
+a == b  
+
---
+++ condicional-if.js
@@ -0,0 +1,9 @@
+let nombre = "David"
+
+if (nombre == "Daniel") {
+    console.log("Hola " + nombre);
+}else if (nombre === "David") {
+    console.log("Hola " + nombre );
+}else {
+    console.log("Nombre no encontrado");
+}
