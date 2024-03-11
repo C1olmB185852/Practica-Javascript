a66cdae1449a43de330fa158470d2f39025e7bc0
C1olmB185852 <dan.ielestebanmat@gmail.com>
2024-03-11 18:51:09 -0500

    .

--- Loop-for-in.js
+++ Loop-for-in.js
@@ -3,6 +3,8 @@
 //de un objeto en JavaScript. Proporciona una forma de acceder 
 //y modificar potencialmente los pares clave-valor dentro de un objeto.
 
+//Ejemplo 1
+
 const listaCompras = {
     leche: 2,
     huevos: 8,
@@ -17,3 +19,18 @@ for (variedad in listaCompras) {
 for (variedad in listaCompras) {
     console.log(`${variedad} : ${listaCompras[variedad]}`);
 }
+
+// Ejemplo 2
+
+console.log("////////////////////////////////////////////////////////");
+
+const persona = {
+    nombre: "Daniel",
+    edad: 19,
+    ciudad: "Armenia"
+  };
+  
+  for (const propiedad in persona) {
+    console.log(propiedad, persona[propiedad]); 
+  }
+  
